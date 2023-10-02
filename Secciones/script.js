71af534230893f26c1ebeaa00815c18a685e2a35
Manuel <manejo03@outlook.es>
2023-10-01 22:54:30 -0500

    Actualizando pequeños detalles del home y del Premium

--- Secciones/script.js
+++ Secciones/script.js
@@ -1,57 +1,58 @@
-document.getElementById("more-options").addEventListener("click", function (event) {
+document.getElementById('more-options').addEventListener("click", function (event) {
     event.preventDefault();
-    var window = document.getElementById("floating-window");
-    if (window.style.display === "none" || window.style.display === "") {
-      window.style.display = "block";
+    var window = document.getElementById('floating-window');
+    if (window.style.display === 'none' || window.style.display === '') {
+        window.style.display = 'block';
     } else {
-      window.style.display = "none";
+        window.style.display = 'none';
     }
     event.stopPropagation();
-  });
+});
 
 document.addEventListener("click", function (event) {
-    var window = document.getElementById("floating-window");
-        if (event.target !== window && event.target !== document.getElementById("more-options")) {
-        window.style.display = "none";
+    var window = document.getElementById('floating-window');
+    if (event.target !== window && event.target !== document.getElementById('more-options')) {
+        window.style.display = 'none';
     }
 });
+
 
 document.getElementById("btn1").addEventListener("click", function (event) {
   var content = document.getElementById("content1");
   var arrow = document.getElementById("img1");
-    if (content.style.display === "none" || content.style.display === "") {
-        content.style.display = "block";
-        arrow.classList.add("rotated");
-    } else {
-        content.style.display = "none";
-        arrow.classList.remove("rotated");
-    }
+  if (content.style.display === "none" || content.style.display === "") {
+    content.style.display = "block";
+    arrow.classList.add("rotated");
+  } else {
+    content.style.display = "none";
+    arrow.classList.remove("rotated");
+  }
   event.stopPropagation(); // Evita que el clic se propague al documento
 });
 
 document.getElementById("btn2").addEventListener("click", function (event) {
   var content = document.getElementById("content2");
   var arrow = document.getElementById("img2");
-    if (content.style.display === "none" || content.style.display === "") {
-        content.style.display = "block";
-        arrow.classList.add("rotated");
-    } else {
-        content.style.display = "none";
-        arrow.classList.remove("rotated");
-    }
+  if (content.style.display === "none" || content.style.display === "") {
+    content.style.display = "block";
+    arrow.classList.add("rotated");
+  } else {
+    content.style.display = "none";
+    arrow.classList.remove("rotated");
+  }
   event.stopPropagation(); // Evita que el clic se propague al documento
 });
 
 document.getElementById("btn3").addEventListener("click", function (event) {
   var content = document.getElementById("content3");
   var arrow = document.getElementById("img3");
-    if (content.style.display === "none" || content.style.display === "") {
-        content.style.display = "block";
-        arrow.classList.add("rotated");
-    } else {
-        content.style.display = "none";
-        arrow.classList.remove("rotated");
-    }
+  if (content.style.display === "none" || content.style.display === "") {
+    content.style.display = "block";
+    arrow.classList.add("rotated");
+  } else {
+    content.style.display = "none";
+    arrow.classList.remove("rotated");
+  }
   event.stopPropagation(); // Evita que el clic se propague al documento
 });
 
